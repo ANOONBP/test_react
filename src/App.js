0ab58fb1c3363f2dac786bb357683d7615e29021
xunxusix <xunxusvongola@gmail.com>
2020-11-23 21:58:44 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const CURRENCY_CHANGER = {
+    type: null,
+    bath: null,
+    dollar: null,
+  };
+
+  const [currency, setCurrency] = useState(CURRENCY_CHANGER);
+  const onInput = (filed) => (event) => {
+    //console.log(field,event.target.value);
+    setCurrency({
+      ...currency,
+      [filed]: event.target.value,
+    });
+  };
+  const onClickCurrency = (filed) => (event) => {
+    if (currency.type === "dollar") {
+      const sum = currency.bath / 33;
+      setCurrency({
+        ...currency,
+        dollar: sum,
+      });
+    } else if (currency.type === "yen") {
+      const sum = currency.bath / 10;
+      setCurrency({
+        ...currency,
+        dollar: sum,
+      });
+    } else if (currency.type === "cny") {
+      const sum = currency.bath * 100;
+      setCurrency({
+        ...currency,
+        dollar: sum,
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <label>เงินบาท</label>
+        <input type="number" value={currency.bath} onChange={onInput("bath")} />
+        =
+        <select onChange={onInput("type")}>
+          <option value="" selected hidden>
+            เลือกสกุลเงิน
+          </option>
+          <option value="dollar">เงินดอลล่า</option>
+          <option value="yen">เงินเยน</option>
+          <option value="cny">เงินหยวน</option>
+        </select>
+        <button type="button" onClick={onClickCurrency(currency.type)}>
+          แปลงค่า
+        </button>
+      </div>
+      <div>
+        <input type="number" value={currency.dollar} readOnly />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
